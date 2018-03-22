233a60c672abcf108a8d7e8095ca29179cbee59c
vilmakoo <vilma.l.kangas@helsinki.fi>
2018-03-22 11:01:00 +0200

    Mv source code to src/

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { StyleSheet, Text, View, Alert, Button } from 'react-native'
 import { StackNavigator } from 'react-navigation'
 
 import Calendar from './components/Calendar'
